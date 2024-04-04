ff2a3591df6ea1af31b724d550449da9ca769135
AliKamar34 <aliekamar123@gmail.com>
2024-04-04 22:37:05 +0200

    adding animation to splash view

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,31 @@
+import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/Utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+
+  @override
+  void initState() {
+    super.initState();
+    navigationView();
+    initAnimation();
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +33,49 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo)
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 6,
+        ),
+        SlideingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
+
+
+
+
+void initAnimation() {
+     animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+
+
+
+  void navigationView() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeView(),
+        duration: kTransitionDuration,
+        transition: Transition.fadeIn,
+      );
+    });
+  }
+
 }
+
