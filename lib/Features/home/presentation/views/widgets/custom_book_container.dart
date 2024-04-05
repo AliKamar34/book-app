3f4212481afb437b38e44d690549b53660370539
AliKamar34 <aliekamar123@gmail.com>
2024-04-06 00:33:24 +0200

    add custom book container & list view

---
+++ lib/Features/home/presentation/views/widgets/custom_book_container.dart
@@ -0,0 +1,24 @@
+import 'package:bookly_app/core/Utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomBookContainer extends StatelessWidget {
+  const CustomBookContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        margin: const EdgeInsets.only(right: 5, left: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.amber,
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/custom_book_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_container.dart';
+import 'package:flutter/material.dart';
+
+class CustomListView extends StatelessWidget {
+  const CustomListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const CustomBookContainer();
+        },
+      ),
+    );
+  }
+}
