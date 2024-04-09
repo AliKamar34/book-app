2eefe58fef0a0a410e962abcc57cf56fc285937f
AliKamar34 <aliekamar123@gmail.com>
2024-04-09 21:51:16 +0200

    implement  fetch newest books method

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,8 +1,8 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
-import 'package:bookly_app/core/errors/faluer_errors.dart';
+import 'package:bookly_app/core/errors/failuer_errors.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
- Future<Either<Faliuer,List<BookModel>>>  fetchBestSellerBooks();
- Future<Either<Faliuer,List<BookModel>>> fetchFeturredBooks();
+  Future<Either<Failuer, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failuer, List<BookModel>>> fetchFeturredBooks();
 }
--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,17 +1,30 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
-import 'package:bookly_app/core/errors/faluer_errors.dart';
+import 'package:bookly_app/core/Utils/api_service.dart';
+import 'package:bookly_app/core/errors/failuer_errors.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Faliuer, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failuer, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.getBooks(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromBookModel(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFalier());
+    }
   }
 
   @override
-  Future<Either<Faliuer, List<BookModel>>> fetchFeturredBooks() {
+  Future<Either<Failuer, List<BookModel>>> fetchFeturredBooks() {
     // TODO: implement fetchFeturredBooks
     throw UnimplementedError();
   }
